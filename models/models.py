140bb8eac0bf875e6929cc14cca8f2737f69d7a3
Jethro Fredericks <jethro.fredericks@countercept.com>
2019-01-23 15:51:46 +0200

    Create `hash_password` and `check_password` methods

--- models/models.py
+++ models/models.py
@@ -1,7 +1,19 @@
 from app import db
+from bcrypt import gensalt, hashpw, checkpw
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(128), nullable=True)
     username = db.Column(db.String(20), nullable=False)
+
+    def hash_password(self, password):
+        self.password = hashpw(password, gensalt())
+    
+
+    def check_password(self, password):
+        return checkpw(password, self.password)
+    
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
