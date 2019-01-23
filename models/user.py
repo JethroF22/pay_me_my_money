2c32c2e0ba07f58d0d7874ca062ec48c0751d5dc
Jethro Fredericks <jethro.fredericks@countercept.com>
2019-01-23 21:49:40 +0200

    Create `UserSchema`

--- models/user.py
+++ models/user.py
@@ -1,9 +1,12 @@
 from app import db
 from bcrypt import gensalt, hashpw, checkpw
+from flask_marshmallow import Marshmallow
+
+ma = Marshmallow()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=True)
     username = db.Column(db.String(20), nullable=False)
 
@@ -17,3 +20,10 @@ class User(db.Model):
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    
+class UserSchema(ma.schema):
+    id = fields.Integer()
+    email = fields.String(required=True)
+    password = fields.String(required=True)
+    username = fields.String(required=True)
