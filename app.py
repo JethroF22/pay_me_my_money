d28bd0306d10a03b31c956b0c7a7bda6e88ede80
Jethro Fredericks <jethro.fredericks@countercept.com>
2019-01-24 11:14:56 +0200

    Create '/user/registration' endpoint'

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
@@ -7,9 +9,13 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+api = Api(app)
 
-@app.route('/')
-def index():
-    return 'Pay Me My Money'
+from resources import UserRegistration
+
+api.add_resource(UserRegistration, '/user/registration')
+
