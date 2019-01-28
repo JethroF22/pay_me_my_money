6b8ca4289bc12f725da0506bf8fdf1623ea9fcdb
Jethro Fredericks <jethro.fredericks@countercept.com>
2019-01-28 11:21:55 +0200

    Add`logout` endpoint, create handler for blacklisted tokens

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
-from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+from datetime import timedelta
+
+from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager, jwt_required, get_raw_jwt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
@@ -12,14 +14,28 @@ load_dotenv()
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
 jwt = JWTManager(app)
 
-from resources import UserRegistration, UserLogin
+from resources import (
+    UserRegistration,
+    UserLogin,
+    LogoutAccessToken)
+from models import RevokedToken
+
 
 api.add_resource(UserRegistration, '/user/registration')
 api.add_resource(UserLogin, '/user/login')
+api.add_resource(LogoutAccessToken, '/logout/')
+
+
+@jwt.token_in_blacklist_loader
+def is_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return RevokedToken.is_token_blacklisted(jti)
 
